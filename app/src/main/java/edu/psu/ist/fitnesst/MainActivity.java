236c572c181913d61a9499965b06c7a4227e79b2
nwh5219 <55903797+nwh5219@users.noreply.github.com>
2022-05-02 20:11:50 -0400

    Dashboard to view exercises

--- app/src/main/java/edu/psu/ist/fitnesst/MainActivity.java
+++ app/src/main/java/edu/psu/ist/fitnesst/MainActivity.java
@@ -3,12 +3,41 @@ package edu.psu.ist.fitnesst;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.EventListener;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+   // ListView lvWorkout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.dashboard);
+
+        //lvWorkout= (ListView)findViewById(R.id.dashboard);
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener((View.OnClickListener) this);
+      //  ArrayList<String> arrayList = new ArrayList<>();
+
+       // arrayList.add("push");
+      //  arrayList.add("pull");
+
+       // ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+       // lvWorkout.setAdapter(arrayAdapter);
+
+
     }
+    @Override
+    public void onClick(View view) {
+        setContentView(R.layout.view_workout);
+    }
+
 }
